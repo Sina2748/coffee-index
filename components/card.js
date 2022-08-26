201ac0aef3e15123838fdba0da16e585e377c567
Flutter PC <sina.sam@gmail.com>
2022-08-26 13:16:43 +0430

    glass

--- components/card.js
+++ components/card.js
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './card.module.css';
-
+// import styles from "./banner.module.css";
+import cls from 'classnames';
+ 
+ 
 const Card = (props) => {
     return (
         <Link href={props.href}>
-            <a>
-                <h2> {props.name}</h2>
-                <Image src={props.imageUrl} height={160} width={260} />
+            <a className={styles.cardLink}>
+            <div className={cls("glass", styles.container)}>
+                <div className={styles.cardHeaderWrapper}>
+                    <h2 className={styles.cardHeader}> {props.name}</h2>
+                </div>
+
+                <div className={styles.cardImageWrapper}>
+                    <Image className={styles.cardImage} src={props.imageUrl} height={160} width={260} />
+                </div>
+            </div>
             </a>
         </Link>
     );
