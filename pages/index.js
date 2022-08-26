210d92420d2e290f152be8ceb19bea706e682a89
Flutter PC <sina.sam@gmail.com>
2022-08-26 13:38:23 +0430

    col

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,11 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400}/>
         </div>
-
-        <Card name="Hello Coffee" imageUrl="/static/hero-image.png" href="/coffee/1" />
+        <div className={styles.cardLayout}>
+          <Card name="Hello Coffee" imageUrl="/static/hero-image.png" href="/coffee/1" className={styles.card}/>
+          <Card name="Hello Coffee" imageUrl="/static/hero-image.png" href="/coffee/1" className={styles.card}/>
+          <Card name="Hello Coffee" imageUrl="/static/hero-image.png" href="/coffee/1" className={styles.card}/>
+        </div>
       </main>
       
 
