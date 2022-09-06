8f3eacf85c87b359964eee384e2cbbadf99374a1
Flutter PC <sina.sam@gmail.com>
2022-09-07 00:01:16 +0430

    8

--- pages/coffee/[dynamic].js
+++ pages/coffee/[dynamic].js
@@ -30,66 +30,73 @@ export async function getStaticProps(staticProps) {
     const params = staticProps.params;
     console.log("params", params);
 
-
+    const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+        return coffeeStore.fsq_id.toString() === params.dynamic;
+      })
 
     return {
       props: {
-        coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.fsq_id.toString() === params.dynamic;
-      }),
+        coffeeStore: findCoffeeStoreById ? findCoffeeStoreById :     {  } ,
      },
     };
   }
 
-const hello = function(props) {
+  const CoffeeStore = (props) => {
     const router = useRouter();
-    
     if (router.isFallback) {
-        return<div>Loading...</div>
-    };
-    
-    const { location, name, neighbourhood, imgUrl } = props.coffeeStore;
-    const handleUpvoteButton = () => {
-        console.log("handle upvote!"); 
+      return <div>Loading...</div>;
     }
-
-    return(
-        <div className= {styles.layout}>
-            <Head>
-                <title>{name}</title>
-            </Head>
-            <div className={styles.container}>
-            <div className={styles.col1}>
-
-                <div className={styles.backToHomeLink}>
-                    <Link href="/"><a>← Back to home</a></Link>  
-                </div>                      
-                <div className={styles.nameWrapper}>
-                    <h1 className={styles.name}>{name}</h1>
-                </div>
-                <Image src={imgUrl}  width={600} height={360} className={styles.storeImg} alt={name}></Image>
+    const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+    const handleUpvoteButton = () => {};
+    return (
+      <div className={styles.layout}>
+        <Head>
+          <title>{name}</title>
+        </Head>
+        <div className={styles.container}>
+          <div className={styles.col1}>
+            <div className={styles.backToHomeLink}>
+              <Link href="/">
+                <a>← Back to home</a>
+              </Link>
             </div>
-            <div className={cls("glass", styles.col2)}>
-                {location.neighborhood[0] && <div className={styles.iconWrapper}>
-                    <Image src="/static/icons/nearMe.svg" width="24" height="24"></Image>                
-                    <p className={styles.text}>{location.neighborhood[0]}</p>
-                </div>}
-
-                <div className={styles.iconWrapper}>
-                    <Image src="/static/icons/places.svg" width="24" height="24"></Image>                
-                    <p className={styles.text}>{location.address}</p>
-                </div>
-
-                <div className={styles.iconWrapper}>
-                    <Image src="/static/icons/star.svg" width="24" height="24"></Image>                
-                    <p className={styles.text}>10</p>
-                </div>
-
-                <button className={styles.upvoteButton} onClick={handleUpvoteButton} >UP Vote!</button>     
+            <div className={styles.nameWrapper}>
+              <h1 className={styles.name}>{name}</h1>
             </div>
+            <Image
+              src={
+                imgUrl ||
+                "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+              }
+              width={600}
+              height={360}
+              className={styles.storeImg}
+              alt={name}
+            />
+          </div>
+          <div className={cls("glass", styles.col2)}>
+            {address && (
+              <div className={styles.iconWrapper}>
+                <Image src="/static/icons/places.svg" width="24" height="24" />
+                <p className={styles.text}>{address}</p>
+              </div>
+            )}
+            {neighbourhood && (
+              <div className={styles.iconWrapper}>
+                <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+                <p className={styles.text}>{neighbourhood}</p>
+              </div>
+            )}
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/star.svg" width="24" height="24" />
+              <p className={styles.text}>1</p>
             </div>
+            <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+              Up vote!
+            </button>
+          </div>
         </div>
-    )
-} 
-
-export default hello;
+      </div>
+    );
+  };
+  export default CoffeeStore;
