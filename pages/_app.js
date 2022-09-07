e07e16996b9d194e56b1318587ddcce51ab924c9
Flutter PC <sina.sam@gmail.com>
2022-09-08 00:21:59 +0430

    11

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import { createContext } from 'react'
 import '../styles/globals.css'
 
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+
+  return (
+    <StoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
 export default MyApp
+ 
