4377e954210b33502c944bcdece0e11659d72a56
Flutter PC <sina.sam@gmail.com>
2022-09-13 18:46:39 +0430

    13

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,59 +1,56 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+import { table, getMinifiedRecords } from "../../lib/airtable";
 
-const table = base('coffee-stores');
-
-// console.log({table});
 
 const createCoffeeStore = async (req, res) => {
     try {
         if (req.method === "POST" ) {
 
             const { id, name, neighbourhood, address, imgUrl, votting } = req.body;
-         
-            // find record
+            if (id) {
+                            // find record
             const findCoffeeStoreRecords = await table.select({
                 filterByFormula: `id=${id}`,
             }).firstPage(); 
 
-            console.log({findCoffeeStoreRecords});
-
             if (findCoffeeStoreRecords.length !== 0 ) {
-
-                const records = findCoffeeStoreRecords.map(record => {
-                    return {
-                        ...record.fields,
-                    }
-                })
+                const records = getMinifiedRecords(findCoffeeStoreRecords);        
                 res.json(records);
+
             } else {
             // creat a record
-            const createRecords = await table.create([
-                {
-                    fields: {
-                        id,
-                        name,
-                        address,
-                        neighbourhood,
-                        votting, 
-                        imgUrl,
-                    }
-                }
-            ]) 
+                if (name) {
+                    const createRecords = await table.create([
+                        {
+                            fields: {
+                                id,
+                                name,
+                                address,
+                                neighbourhood,
+                                votting, 
+                                imgUrl,
+                            }
+                        } 
+                    ])  
 
-            const records = createRecords.map(record => {
-                return {
-                    ...record.fields,
+                    const records = getMinifiedRecords(createRecords); 
+                    res.json({ records })
+
+                } else {
+                    res.status(400);
+                    res.json({ message: "name is missing"});
                 }
-            })
-            res.json({ records })
 
             } 
+            } else {
+                res.status(400);
+                res.json({ message: "id is missing"});
+            }
+
         }
     } catch(err) {
-        console.error('Error finding store', err);
+        console.error('Error creating or finding store', err);
         res.status(500);
-        res.json({message: 'Error finding store', err});
+        res.json({message: 'Error creating or finding store', err});
     } 
     
 }
