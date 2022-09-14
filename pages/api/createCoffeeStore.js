cf2c7af69e58b4571774a5226070734033da6ebe
Flutter PC <sina.sam@gmail.com>
2022-09-15 03:02:18 +0430

    4-refactor

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,4 @@
-import { table, getMinifiedRecords } from "../../lib/airtable";
+import { table, getMinifiedRecords, findRecordByFilter } from "../../lib/airtable";
 
 
 const createCoffeeStore = async (req, res) => {
@@ -8,12 +8,9 @@ const createCoffeeStore = async (req, res) => {
             const { id, name, neighbourhood, address, imgUrl, votting } = req.body;
             if (id) {
             // find record
-            const findCoffeeStoreRecords = await table.select({
-                filterByFormula: `id="${id}"`,
-            }).firstPage(); 
+            const records = await findRecordByFilter(id);
 
-            if (findCoffeeStoreRecords.length !== 0 ) {
-                const records = getMinifiedRecords(findCoffeeStoreRecords);        
+            if (records.length !== 0 ) {              
                 res.json(records);
 
             } else {
--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,11 +1,26 @@
-const getCoffeeStoreById = (req, res) => {
+import { table, getMinifiedRecords, findRecordByFilter } from "../../lib/airtable";
+
+
+const getCoffeeStoreById = async (req, res) => {
      
     const {id} = req.query;
 
     try {
         if(id) {
-            res.json({ message: `id is created ${id}` })
-            
+
+            const records = await findRecordByFilter(id);
+
+            if (records.length !== 0 ) {              
+                res.json(records);
+            } else {
+                res.json({ message: `id ${id} could not be found!` })
+            }
+
+
+
+
+           
+             
         } else {
             res.status(400);
             res.json({ message: "ID us missing"})
