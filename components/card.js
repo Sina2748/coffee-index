afb6eb3051c29825e757ef8ae870b9abe32c4c07
Flutter PC <sina.sam@gmail.com>
2022-09-17 02:01:26 +0430

    tailwind installed

--- components/card.js
+++ components/card.js
@@ -1,21 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './card.module.css';
-// import styles from "./banner.module.css";
-import cls from 'classnames';
+
  
  
 const Card = (props) => {
     return (
         <Link href={props.href}>
-            <a className={styles.cardLink}>
-            <div className={cls("glass", styles.container)}>
-                <div className={styles.cardHeaderWrapper}>
-                    <h2 className={styles.cardHeader}> {props.name}</h2>
+            <a className="">
+            <div className="">
+                <div className="">
+                    <h2 className=""> {props.name}</h2>
                 </div>
 
-                <div className={styles.cardImageWrapper}>
-                    <Image className={styles.cardImage} src={props.imageUrl} height={160} width={260} />
+                <div className="">
+                    <Image className="" src={props.imageUrl} height={160} width={260} />
                 </div>
             </div>
             </a>
