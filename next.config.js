5f79fa8d510d2d07f50f7080440bd986031f51bb
Flutter PC <sina.sam@gmail.com>
2022-09-16 17:26:21 +0430

    adding unspash hust to config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // images: {
+  //   domains: ['images.unsplash.com'],
+  // },
+}
+
+// module.exports = nextConfig
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['images.unsplash.com'],
   },
 }
-
-module.exports = nextConfig
